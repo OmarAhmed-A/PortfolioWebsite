a752ce6c19f281855508dd2e70399c5c7ab0798f
Omar Ahmed <omarahmed170@gmail.com>
2023-01-19 02:02:08 +0200

    edit Resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "I am a highly motivated learner, constantly looking to learn new technologies and apply them to real-world problems. I thrive in team environments and enjoy finding new solutions and workarounds to obstacles that can be better suited to the specific issue at hand. I hope to learn about different roles within the industry and how they fit together.",
   resumeLink:
-    "https://drive.google.com/file/d/19O64Oj-1ntv_veYoCNuB-8a47TTdtT06/view?usp=sharing",
+    "https://drive.google.com/file/d/1sJKjo0s1rSi1wy6sgTtPbweQ2Ta71AS5/view?usp=share_link",
   portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
   githubProfile: "https://github.com/OmarAhmed-A",
 };
